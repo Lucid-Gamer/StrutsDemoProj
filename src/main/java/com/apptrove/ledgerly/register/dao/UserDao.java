3fe3f243b80c955b0181f92efb14c3db6b444dc1
Lucid-Gamer <aditya.96.nair@gmail.com>
2024-12-01 23:12:26 +0530

    User Entity Created

---
+++ src/main/java/com/apptrove/ledgerly/register/dao/UserDao.java
@@ -0,0 +1,9 @@
+package com.apptrove.ledgerly.register.dao;
+
+import com.apptrove.ledgerly.admin.models.User;
+
+public interface UserDao {
+
+	public User registerUser(User newUser);
+	
+}
---
+++ src/main/java/com/apptrove/ledgerly/register/dao/UserDaoImplementation.java
@@ -0,0 +1,21 @@
+package com.apptrove.ledgerly.register.dao;
+
+import com.apptrove.ledgerly.admin.models.User;
+
+public class UserDaoImplementation implements UserDao {
+
+	@Override
+	public User registerUser(User newUser) {
+		User user = new User();
+		try {
+			
+		} catch (Exception e) {
+			
+		}
+		
+		return null;
+	}
+	
+	
+
+}
